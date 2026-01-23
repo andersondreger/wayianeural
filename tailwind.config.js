e1f0ff63c1f5cf5e46aa57bf68631829c1b862f9
Dreger <andersondreger@hotmail.com>
2026-01-23 18:35:22 -0300

    feat: Update project dependencies and configurations
    Updates project dependencies to align with Next.js 14 and Tailwind CSS 3.4. This includes adjusting tsconfig.json and introducing postcss.config.js for optimal styling pipeline. The build and deployment configurations in package.json are also streamlined.

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./views/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
